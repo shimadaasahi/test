4a1b27a5dd15677cd6501276befe930509b79350
shimadaasahi <b1021163@fu.ac.jp>
2023-05-16 21:20:51 +0900

    to change word

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> It id</Text>
+      <Text> It is hard</Text>
       <StatusBar style="auto" />
     </View>
   );
